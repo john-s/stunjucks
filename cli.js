5996fb5e49047fe3c8336b5f2395c591a14ced7a
John Schulte <john@fanbase.com>
2017-08-02 20:18:09 -0700

    improve comments

--- cli.js
+++ cli.js
@@ -4,12 +4,13 @@
 
 var stunjucks = require('./index.js');
 
-// default
+// default file name
 var fileName = 'stunjucks.config.js';
 if (process.argv.length > 2) {
     fileName = process.argv[2]
 }
 var currentDir = process.cwd();
+// only call stunjucks if the config is loaded successfully
 var successfulConfig = true;
 try {
     config = require(currentDir + '/' + fileName);
